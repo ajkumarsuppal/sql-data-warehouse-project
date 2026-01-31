4e2913c6427b662e79db8484c35ec14be0cc3417
Ajay Kumar Suppal <akrsuppal@gmail.com>
2026-01-31 15:18:33 +0530

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'. It also includes a warning about data loss if the database already exists.

---
+++ scripts/init_database.sql
@@ -0,0 +1,44 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas: its like a folder or container that helps us keep things organized. we decided on medallion architecture. so we got 3 layers - gold,silver,bronze.
+--we are going to create schema for each layer
+--in ssms, go to DataWarehouse->Security->Schemas->you will find the bronze schema created
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
